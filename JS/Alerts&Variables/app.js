c8faa0a1ed0975604b62b61cfd0b6c56440ead91
Bilal raza <attari1235@gmail.com>
2024-02-20 10:17:05 +0500

    js started

---
+++ JS/Alerts&Variables/app.js
@@ -0,0 +1,43 @@
+
+// javascript ka bana banaya function he jo pop up openn krdeta he
+// alert('Hello World!')
+
+
+// javascript mein ham variables banate hen data ko store krne ke lye.
+
+// Date ki 8 types hen
+// 1.String , jo single quote ya double quote mein ae e.g "Hello", 'Hello'
+// 2.Number , number ho or single quote ya double quote na ho e.g 20 , 1000 
+// 3.Boolean , true , false 
+// 4.Undefined ,variable banaya lekin usko value na di ho
+// 5.Function
+// 6.Array
+// 7.Object
+// 8.null
+
+
+// var banane ka tariqa
+// var ka keyword likhenge phr space deke variable ka naam likhenge jo data save horaha he 
+// uske hisaab se hoga , phr = lagake value dedenge
+
+
+var greetings = "Hello World"  //string
+var noOfStudents = 50 // number 
+var isClassGoingOn = true // boolean
+var noOfStudentsPassed //value will be undefined
+
+alert(greetings)
+alert(noOfStudents)
+alert(isClassGoingOn)
+alert(noOfStudentsPassed)
+
+// delete , for , function , if , else
+var first_name = 'Bilal'
+
+// variable ka naam rakhne ke kuch rules hen
+// 1.variable ka naam number se start nahn hosakta , e.g var 1stname
+// 2.variable ka naam mein space nahn hoga
+// 3.variable ke naam mein underscore or dollar ke elawa koi or special character 
+    // nahn aega.
+// 4.variable ka naam javascript ke keyword ke naam par na ho.
+// 5.best practice ye he ke naam camelCase   mein likha jae
