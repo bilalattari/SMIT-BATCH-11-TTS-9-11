519fed1ebb6998295f1b1eb47170db9849672a8c
Muhammad Ahmed Raza <121611274+ahmedrazaaligithub@users.noreply.github.com>
2024-03-05 13:42:32 +0500

    uncommit app.js

--- JS/if else/app.js
+++ JS/if else/app.js
@@ -1,39 +1,39 @@
-// if(condition){
-//     result
-// }
+ if(condition){
+     result
+ }
 
-// var age = +prompt("write your age")
-// if (age >= 18){
-//     console.log("admission hogaya");
-//     document.write("admission hogaya")
-// }
-// else{
-//    console.log("admission nhi hua") 
-//    document.write("<h1>admission nhi hogaya</h1>")
-// }
-// if html && css quiz number > 150 so you are eligible else not 
+ var age = +prompt("write your age")
+ if (age >= 18){
+     console.log("admission hogaya");
+     document.write("admission hogaya")
+ }
+  else{
+     console.log("admission nhi hua") 
+     document.write("<h1>admission nhi hogaya</h1>")
+  }
+  if html && css quiz number > 150 so you are eligible else not 
 
-// var html = +prompt("write your html numbers")
-// var css = +prompt("write your css numbers")
-// var result = html + css
-// console.log(result);
-// if(result >=150){
-//     console.log("pass");
-// }
-// else{
-//     console.log("fail");
-// }
+  var html = +prompt("write your html numbers")
+  var css = +prompt("write your css numbers")
+  var result = html + css
+  console.log(result);
+  if(result >=150){
+      console.log("pass");
+  }
+  else{
+      console.log("fail");
+  }
 
 
-// Comparison operators
-// value chek krne ke lye == use hoga or value + datatype check krne ke lye === use hoga 
+  Comparison operators
+  //value chek krne ke lye == use hoga or value + datatype check krne ke lye === use hoga 
 var number = 20
-// if (number==="20"){
-// console.log("equal");
-// }
-// else{
-//     console.log("not equal");
-// }
+  if (number==="20"){
+  console.log("equal");
+  }
+  else{
+      console.log("not equal");
+  }
 if (number !== 20) {
     console.log("not equal")
 }
