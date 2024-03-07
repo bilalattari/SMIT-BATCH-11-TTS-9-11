2ef7daf1e93f4b2f74fc229f5f2ba8f951afa394
Muhammad Ahmed Raza <121611274+ahmedrazaaligithub@users.noreply.github.com>
2024-03-07 11:00:57 +0500

    else if

--- JS/if else/app.js
+++ JS/if else/app.js
@@ -1,42 +1,129 @@
- if(condition){
-     result
- }
+ // if(condition){
+//     result
+// }
 
- var age = +prompt("write your age")
- if (age >= 18){
-     console.log("admission hogaya");
-     document.write("admission hogaya")
- }
-  else{
-     console.log("admission nhi hua") 
-     document.write("<h1>admission nhi hogaya</h1>")
-  }
-  //if html && css quiz number > 150 so you are eligible else not 
+// var age = +prompt("write your age")
+// if (age >= 18){
+//     console.log("admission hogaya");
+//     document.write("admission hogaya")
+// }
+// else{
+//    console.log("admission nhi hua") 
+//    document.write("<h1>admission nhi hogaya</h1>")
+// }
+// if html && css quiz number > 150 so you are eligible else not 
 
-  var html = +prompt("write your html numbers")
-  var css = +prompt("write your css numbers")
-  var result = html + css
-  console.log(result);
-  if(result >=150){
-      console.log("pass");
-  }
-  else{
-      console.log("fail");
-  }
+// var html = +prompt("write your html numbers")
+// var css = +prompt("write your css numbers")
+// var result = html + css
+// console.log(result);
+// if(result >=150){
+//     console.log("pass");
+// }
+// else{
+//     console.log("fail");
+// }
+
+// Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
 
 
-  Comparison operators
-  //value chek krne ke lye == use hoga or value + datatype check krne ke lye === use hoga 
-var number = 20
-  if (number==="20"){
-  console.log("equal");
-  }
-  else{
-      console.log("not equal");
-  }
-if (number !== 20) {
-    console.log("not equal")
-}
-else {
-    console.log("equal");
-}
+// var num1 = +prompt("enter your 1st integer");
+// var num2 = +prompt("enter your 2nd integer");
+
+// if(num1 > num2){
+//     console.log(num1 + " is greater then " + num2);
+// }
+
+// else{
+//     console.log(num2 + " is greater then " + num1);
+// }
+
+// Write a program that takes input a number from user &
+// state whether the number is positive, negative or zero.
+
+// var num = +prompt("Enter Your Number")
+
+// if (num > 0){
+//     console.log("positive");
+// }
+// else if (num === 0){
+//     console.log("neutral");
+// }
+// else{
+//     console.log("negative");
+// }
+// var num1 = +prompt("enter 1st number")
+// var num2 = +prompt("enter 2nd number")
+// var operator = prompt("enter your operator", '+ - * /')
+// var result;
+// if(operator == "+"){
+//     result = num1 + num2
+//     console.log(result);
+// }else if (operator == "-"){
+//     result = num1 - num2
+//     console.log(result);
+// }else if (operator == "*"){
+//     result = num1 * num2
+//     console.log(result);
+// }else if (operator == "/"){
+//     result = num1 / num2
+//     console.log(result);
+// }else{
+//     alert("your operator does not exist")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Comparison operators
+//==
+//===
+//!=
+//!==
+//>
+//<
+//  &&
+//  ||
+
+// var number = 20
+// if (number === 20){
+// console.log("equal");
+// }
+// else{
+// console.log("not equal");
+// }
+// if (number != 30) {
+//     console.log("not equal")
+// }
+// else {
+//     console.log("equal");
+// }
+
+// if & else if 
+var age = +prompt("enter age")
+var clas = +prompt("enter time")
+if (age > 8 && clas < 3 ) {
+    alert("Come to our school!");
+     }
+     else {
+     alert("dont Come to our school!");
+     }
+var weight = +prompt("enter weight")
+var time = +prompt("enter time")
+if (weight > 300 || time < 6) {
+    alert("Come to our tryout!");
+     }
+     else {
+     alert("Come to our cookout!");
+     }
