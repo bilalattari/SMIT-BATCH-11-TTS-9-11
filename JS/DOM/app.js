1d3a7aa87554f7a45db3cc83c5481550f1fba1fb
Muhammad Ahmed Raza <121611274+ahmedrazaaligithub@users.noreply.github.com>
2024-05-28 11:03:03 +0500

    object & foreach

--- JS/DOM/app.js
+++ JS/DOM/app.js
@@ -4,19 +4,71 @@
 // console.log("body=>", body[0].children[0].children[0])
 
 
-var btn = document.getElementById('addBtn')
-var historyDiv = document.getElementById('historyDiv')
-var todo = document.getElementById('todo')
+// var btn = document.getElementById('addBtn')
+// var historyDiv = document.getElementById('historyDiv')
+// var todo = document.getElementById('todo')
 
-btn.addEventListener('click', function () {
-    if (todo.value.trim() === '') return alert('Please add todo.')
+// btn.addEventListener('click', function () {
+//     if (todo.value.trim() === '') return alert('Please add todo.')
 
-    var now = new Date()
-    var time = now.toLocaleTimeString()
+//     var now = new Date()
+//     var time = now.toLocaleTimeString()
 
-    var li = `<li>${todo.value.trim()} ${time} <button onclick = "console.log(this.parentNode.remove())">Del </button>  </li>`
-    historyDiv.innerHTML += li
+//     var li = `<li>${todo.value.trim()} ${time} <button onclick = "console.log(this.parentNode.remove())">Del </button>  </li>`
+//     historyDiv.innerHTML += li
 
-    todo.value = ''
+//     todo.value = ''
 
+// })
+
+
+// console.log(document)
+
+
+// var parent = document.getElementById('parent')
+// var child3 = document.getElementById('child3')
+
+// console.log("parent=>", parent)
+// console.log("children of parent=>", parent.children)
+// console.log("dada abu of parent=>", parent.parentElement)
+// console.log("3sre number ka", child3)
+// console.log("is se pehle wala", child3.previousElementSibling)
+// console.log("is ke baad wala", child3.nextElementSibling)
+
+
+
+var input = document.getElementById('todo')
+var add_btn = document.getElementById('add_btn')
+var list = document.getElementById('list')
+var delete_all_btn = document.getElementById('delete_all_btn')
+
+
+delete_all_btn.addEventListener('click', function () {
+    list.innerHTML = ''
 })
+
+add_btn.addEventListener('click', function () {
+    var val = input.value.trim()
+    if (!val) return alert('Sahi se item likhlo pehle')
+    var li = `<li>
+<span>${val}</span> 
+<button onclick="edit(this)">Edit</button> 
+<button onclick="deleteTodo(this)">Del</button> </li>`
+    list.innerHTML += li
+
+    input.value = ''
+})
+
+function edit(element) {
+    console.log(element.previousElementSibling.innerText)
+
+    var updated = prompt('Update value', element.previousElementSibling.innerText)
+
+    element.previousElementSibling.innerText = updated
+}
+
+
+function deleteTodo(element) {
+    console.log("element=>", element)
+    element.parentElement.remove()
+}
